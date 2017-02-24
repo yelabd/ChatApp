cf7c3a6d02b4de4b29700e4cb0846f0e1553dccb
Youssef <yelabd@purdue.edu>
2017-02-23 19:59:29 -0500

    Done

---
+++ ChatApp/MessageViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageViewCell.swift
+//  ChatApp
+//
+//  Created by Youssef Elabd on 2/23/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
