0c68314c997f4f82e1a6dba5824ebd097ec1859d
Youssef <yelabd@purdue.edu>
2017-02-28 16:34:15 -0500

    Fix

--- ChatApp/ChatViewController.swift
+++ ChatApp/ChatViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    var messages : [PFObject] = []
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 150
+        
+        
+        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(ChatViewController.query), userInfo: nil, repeats: true)
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +33,71 @@ class ChatViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onSend(_ sender: Any) {
+        let message = PFObject(className: "Message")
+        message["text"] = messageTextfield.text
+        message.saveInBackground { (succes: Bool, error: Error?) in
+            if let error = error{
+                print(error)
+            }
+            else{
+                print("Yaas")
+            }
+        }
+        
+    }
+    
+    func query(){
+        var query = PFQuery(className: "Message")
+        query.findObjectsInBackground {
+            (objects: [PFObject]?, error: Error?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) scores.")
+                // Do something with the found objects
+                if let objects = objects {
+                    self.messages = objects
+                }
+            } else {
+                
+                // Log details of the failure
+                print("Error: \(error!)")
+            }
+        }
+        
+        tableView.reloadData()
+
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! MessageViewCell
+        
+        let row = indexPath.row
+        
+       let message = messages[row]
+        
+        
+        //        if (businessInfo.rating < 4.0 && businessInfo.rating > 2){
+        //            let yellowColor = UIColor(red: 255/255.0, green: 223/255.0, blue: 0/255.0, alpha: 1.0)
+        //            cell.ratingView.backgroundColor = yellowColor
+        //        }else if(businessInfo.rating <= 2){
+        //            cell.ratingView.backgroundColor = UIColor.red
+        //        }
+        
+        
+        cell.messageLabel.text = message["text"] as? String
+        
+        return cell
+    }
+
 
     /*
     // MARK: - Navigation
--- ChatApp/MessageViewCell.swift
+++ ChatApp/MessageViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MessageViewCell: UITableViewCell {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
